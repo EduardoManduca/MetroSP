1d9f3f2163f313873745dabc38059465114823a2
Caio Onha Ferreira <caioonha@gmail.com>
2025-05-13 08:39:40 -0300

    Criando tela menu

--- src/teste_tkainter.py
+++ src/teste_tkainter.py
@@ -1,10 +1,13 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
+from models.UserModel import UserModel
+from DataBase.LoginDAO import LoginDAO
 
 # Configuração inicial
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 
+# ======================================= Tela Login ======================================= 
 app = ctk.CTk()
 app.geometry("1300x700")
 app.title("Login - Metrô SP")
@@ -39,9 +42,61 @@ entry_senha.place(relx=0.5, y=453, anchor="center")
 # Botão Entrar 
 def login():
     print(f"ID: {entry_id.get()}, Senha: {entry_senha.get()}")
+    usuario = UserModel(entry_id.get(), entry_senha.get())
+    uDAO = LoginDAO()
+    resultado = uDAO.login(usuario)
+    
+    if resultado:
+        print(True)
+        abrir_menu()
+    else:
+        print(False)
 
 btn_login = ctk.CTkButton(app, text="Entrar", width=349, height=53, corner_radius=10, fg_color="#001489",font=("Arial", 14), command=login)
 btn_login.place(relx=0.5, y=590, anchor="center")
+
+# =====================================================================================================================
+
+# ============================= Tela Menu =============================
+def abrir_menu():
+    for winget in app.winfo_children():
+        winget.destroy()
+
+    # ======== Fundo =========
+    img_fundo = Image.open("./imgs/fundo_menu.png").resize((1300, 700))
+    bg_image = ImageTk.PhotoImage(img_fundo)
+
+    bg_label = ctk.CTkLabel(app, image=bg_image, text="")
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # ====== Botões ======
+    btn_iniciar = ctk.CTkButton(
+        app,
+        text="Iniciar",
+        width=349,
+        height=53,
+        font=("Arial", 20),
+        corner_radius=10,
+        fg_color="#001489",
+        hover_color="#001a73",
+        text_color="white",
+        command=lambda: print("Iniciar clicado")
+    )
+    btn_iniciar.place(relx=0.5, y=420, anchor="center")
+
+    btn_historico = ctk.CTkButton(
+        app,
+        text="Histórico",
+        width=349,
+        height=53,
+        font=("Arial", 20),
+        corner_radius=10,
+        fg_color="#001489",
+        hover_color="#001a73",
+        text_color="white",
+        command=lambda: print("Histórico clicado")
+    )
+    btn_historico.place(relx=0.5, y=508, anchor="center")
 
 app.mainloop()
 
