18e7bb012988050da3675d6116ba36296679725a
EduardoManduca <eduardo.aranha.manduca@gmail.com>
2025-05-26 08:19:42 -0300

    Update PortaLacrada.py

--- src/Game/PortaLacrada.py
+++ src/Game/PortaLacrada.py
@@ -2,7 +2,7 @@ class PortaLacrada:
     def __init__(self, estado: bool = False):
         self.estado = estado
         
-    def get_estaado(self) -> bool:
+    def get_estado(self) -> bool:
         return self.estado
     
     def set_estado(self, estado: bool):
